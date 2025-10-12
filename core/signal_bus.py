705fc29ae462e3c7a8dd2537826b4d822bb16c7d
GitLab CI <ci@gitlab.com>
2025-10-11 19:06:39 -0700

    [update] signaling

---
+++ core/signal_bus.py
@@ -0,0 +1,10 @@
+import PySide6.QtCore as qtc
+
+
+class SignalBus(qtc.QObject):
+    launchThread = qtc.Signal(object)
+    terminateThread = qtc.Signal(str)
+    stopThread = qtc.Signal(str)
+
+
+signalBus = SignalBus()
